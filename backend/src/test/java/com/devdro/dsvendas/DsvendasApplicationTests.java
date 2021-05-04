d2c2e3c6cfa041eeebc4cd625aa30fd93527bc17
dev-dro <danilo.ro@protonmail.com>
2021-05-04 20:23:17 -0300

    Project created

---
+++ backend/src/test/java/com/devdro/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdro.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
